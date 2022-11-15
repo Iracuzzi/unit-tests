ee31fda39ce2403a8e369c69b466a195a57b80c5
Iracuzzi <arnau.monforte@gmail.com>
2022-11-15 14:24:15 +0100

    Tasted solucions

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,3 @@
 export const hackerSpeak = (words) => {
-  return words
+  return words.split("").map((letter) => letter === 'a' || letter === 'A' ? letter = 4 : letter === 'e' || letter === 'E' ? letter = 3 : letter === 'o' || letter === 'O' ? letter = 0 : letter === 'i' || letter === 'I' ? letter = 1 : letter === 's' || letter === 'S' ? letter = 5 :  letter ).join('')
 }
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,5 @@
 export const hashtags = (text) => {
-  return text
+  const hashtags = []
+  text.split(' ').map((word)=> word[0] === '#' ? hashtags.push(word) : word)
+  return hashtags
 }
